51fb569f9ecad969ed46909f001dcde03455c9da
franciscovillaescusa <villaescusa.francisco@gmail.com>
2022-04-05 13:27:24 -0400

    update

--- plots/comparison_same_seed/plot_CUBEP3M.py
+++ plots/comparison_same_seed/plot_CUBEP3M.py
@@ -154,6 +154,7 @@ f4 = '%s/Pk_CUBEP3M_nc512_pp2.txt'%root
 f5 = '%s/Pk_CUBEP3M_nc1024_pp0.txt'%root
 f6 = '%s/Pk_CUBEP3M_nc1024_pp2.txt'%root
 f7 = '%s/Pk_CUBEP3M_nc1024_pp2_rsoft05.txt'%root
+f8 = '%s/Pk_CUBEP3M_nc2048_pp2.txt'%root
 
 data1 = np.loadtxt(f1) 
 data2 = np.loadtxt(f2) 
@@ -162,6 +163,7 @@ data4 = np.loadtxt(f4)
 data5 = np.loadtxt(f5) 
 data6 = np.loadtxt(f6) 
 data7 = np.loadtxt(f7) 
+data8 = np.loadtxt(f8) 
 
 ax2.fill_between([x_min,x_max],[1.01,1.01],[0.99,0.99],color='k',alpha=0.3)
 ax2.fill_between([x_min,x_max],[1.02,1.02],[0.98,0.98],color='k',alpha=0.1)
@@ -171,8 +173,9 @@ p2,=ax1.plot(data2[:,0],data2[:,1],linestyle='-',marker='None',c='b')
 p3,=ax1.plot(data3[:,0],data3[:,1],linestyle='-',marker='None',c='g')
 p4,=ax1.plot(data4[:,0],data4[:,1],linestyle='-',marker='None',c='purple')
 p5,=ax1.plot(data5[:,0],data5[:,1],linestyle='-',marker='None',c='brown')
-p6,=ax1.plot(data6[:,0],data5[:,1],linestyle='-',marker='None',c='cyan')
-p7,=ax1.plot(data7[:,0],data5[:,1],linestyle='-',marker='None',c='gold')
+p6,=ax1.plot(data6[:,0],data6[:,1],linestyle='-',marker='None',c='cyan')
+p7,=ax1.plot(data7[:,0],data7[:,1],linestyle='-',marker='None',c='gold')
+p8,=ax1.plot(data8[:,0],data8[:,1],linestyle='-',marker='None',c='pink')
 ax2.plot(data2[:,0],data1[:,1]/data1[:,1],linestyle='-',marker='None',c='r')
 ax2.plot(data2[:,0],data2[:,1]/data1[:,1],linestyle='-',marker='None',c='b')
 ax2.plot(data2[:,0],data3[:,1]/data1[:,1],linestyle='-',marker='None',c='g')
@@ -180,6 +183,7 @@ ax2.plot(data2[:,0],data4[:,1]/data1[:,1],linestyle='-',marker='None',c='purple'
 ax2.plot(data2[:,0],data5[:,1]/data1[:,1],linestyle='-',marker='None',c='brown')
 ax2.plot(data2[:,0],data6[:,1]/data1[:,1],linestyle='-',marker='None',c='cyan')
 ax2.plot(data2[:,0],data7[:,1]/data1[:,1],linestyle='-',marker='None',c='gold')
+ax2.plot(data2[:,0],data8[:,1]/data1[:,1],linestyle='-',marker='None',c='pink')
 
 ax1.plot([32,32],[y_min,y_max],linestyle='--',marker='None',c='k')
 ax2.plot([32,32],[0.9,1.03],linestyle='--',marker='None',c='k')
@@ -190,9 +194,10 @@ ax2.plot([32,32],[0.9,1.03],linestyle='--',marker='None',c='k')
 ax1.text(0.85,0.9, r"$z=0$", fontsize=18, color='k',transform=ax1.transAxes)
 
 #legend
-ax1.legend([p1,p2,p3,p4,p5,p6,p7],
+ax1.legend([p1,p2,p3,p4,p5,p6,p7,p8],
            ["Gadget", "nc512_pp0", "nc512_pp1", "nc512_pp2",
-            "nc1024_pp0", "nc1024_pp2", "nc1024_pp2_rsoft0.5"],
+            "nc1024_pp0", "nc1024_pp2", "nc1024_pp2_rsoft0.5",
+            "nc2048_pp2"],
            loc=0,prop={'size':12},ncol=1,frameon=True)
             
             #columnspacing=2,labelspacing=2)
