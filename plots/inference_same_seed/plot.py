eed137af8bfa45dcaa98ab67058eb6a9722b8299
franciscovillaescusa <villaescusa.francisco@gmail.com>
2022-04-08 21:06:08 -0400

    update

--- plots/inference_same_seed/plot.py
+++ plots/inference_same_seed/plot.py
@@ -152,12 +152,13 @@ ax2.plot([-10,200],[0.834, 0.834],  lw=1, c='k')
 root = '/mnt/ceph/users/fvillaescusa/Nbody_systematics/PUBLIC/Codes/train_test_models'
 
 f1 = '%s/Trained_Gadget_tested_Gadget_z=0.00.txt'%root
-f2 = '%s/Trained_Gadget_tested_PKDGrav_z=0.00.txt'%root
-f3 = '%s/Trained_Gadget_tested_PKDGrav_HR_z=0.00.txt'%root
-f4 = '%s/Trained_Gadget_tested_Abacus_z=0.00.txt'%root
-f5 = '%s/Trained_Gadget_tested_Ramses_z=0.00.txt'%root
-f6 = '%s/Trained_Gadget_tested_Ramses_HR_z=0.00.txt'%root
-f7 = '%s/Trained_Gadget_tested_CUBEP3M_z=0.00.txt'%root
+f2 = '%s/Trained_Gadget_tested_Gadget_HR_z=0.00.txt'%root
+f3 = '%s/Trained_Gadget_tested_PKDGrav_z=0.00.txt'%root
+f4 = '%s/Trained_Gadget_tested_PKDGrav_HR_z=0.00.txt'%root
+f5 = '%s/Trained_Gadget_tested_Abacus_z=0.00.txt'%root
+f6 = '%s/Trained_Gadget_tested_Ramses_z=0.00.txt'%root
+f7 = '%s/Trained_Gadget_tested_Ramses_HR_z=0.00.txt'%root
+f8 = '%s/Trained_Gadget_tested_CUBEP3M_z=0.00.txt'%root
 
 data1 = np.loadtxt(f1) 
 data2 = np.loadtxt(f2) 
@@ -166,6 +167,7 @@ data4 = np.loadtxt(f4)
 data5 = np.loadtxt(f5) 
 data6 = np.loadtxt(f6) 
 data7 = np.loadtxt(f7) 
+data8 = np.loadtxt(f8) 
 
 elems = 15
 x = np.arange(elems)
@@ -173,57 +175,62 @@ indexes = x*8
 
 p1=ax1.errorbar(x,data1[indexes,6],yerr=data1[indexes,12],lw=1,fmt='o',ms=2,
                 elinewidth=1,capsize=5,linestyle='None', c='b') 
+p2=ax1.errorbar(x,data2[indexes,6],yerr=data2[indexes,12],lw=1,fmt='o',ms=2,
+                elinewidth=1,capsize=5,linestyle='None', c='darkblue') 
 
-p2=ax1.errorbar(x+elems*1+1,data2[indexes,6],yerr=data2[indexes,12],lw=1,fmt='o',ms=2,
-                elinewidth=1,capsize=5,linestyle='None',c='r') 
 p3=ax1.errorbar(x+elems*1+1,data3[indexes,6],yerr=data3[indexes,12],lw=1,fmt='o',ms=2,
-                elinewidth=1,capsize=5,linestyle='None', c='darkred') 
-
-p4=ax1.errorbar(x+elems*2+2,data4[indexes,6],yerr=data4[indexes,12],lw=1,fmt='o',ms=2,
-                elinewidth=1,capsize=5,linestyle='None', c='purple')
- 
-p5=ax1.errorbar(x+elems*3+3,data5[indexes,6],yerr=data5[indexes,12],lw=1,fmt='o',ms=2,
-                elinewidth=1,capsize=5,linestyle='None',c='lightgreen') 
-p6=ax1.errorbar(x+elems*3+3,data6[indexes,6],yerr=data6[indexes,12],lw=1,fmt='o',ms=2,
-                elinewidth=1,capsize=5,linestyle='None',c='darkgreen') 
-
-p7=ax1.errorbar(x+elems*4+4,data7[indexes,6],yerr=data7[indexes,12],lw=1,fmt='o',ms=2,
-                elinewidth=1,capsize=5,linestyle='None', c='orange') 
-
-
-
-p1=ax2.errorbar(x,data1[indexes,7],yerr=data1[indexes,13],lw=1,fmt='o',ms=2,
-                elinewidth=1,capsize=5,linestyle='None', c='b') 
-
-p2=ax2.errorbar(x+elems*1+1,data2[indexes,7],yerr=data2[indexes,13],lw=1,fmt='o',ms=2,
                 elinewidth=1,capsize=5,linestyle='None',c='r') 
-p3=ax2.errorbar(x+elems*1+1,data3[indexes,7],yerr=data3[indexes,13],lw=1,fmt='o',ms=2,
+p4=ax1.errorbar(x+elems*1+1,data4[indexes,6],yerr=data4[indexes,12],lw=1,fmt='o',ms=2,
                 elinewidth=1,capsize=5,linestyle='None', c='darkred') 
 
-p4=ax2.errorbar(x+elems*2+2,data4[indexes,7],yerr=data4[indexes,13],lw=1,fmt='o',ms=2,
+p5=ax1.errorbar(x+elems*2+2,data5[indexes,6],yerr=data5[indexes,12],lw=1,fmt='o',ms=2,
                 elinewidth=1,capsize=5,linestyle='None', c='purple')
  
-p5=ax2.errorbar(x+elems*3+3,data5[indexes,7],yerr=data5[indexes,13],lw=1,fmt='o',ms=2,
+p6=ax1.errorbar(x+elems*3+3,data6[indexes,6],yerr=data6[indexes,12],lw=1,fmt='o',ms=2,
                 elinewidth=1,capsize=5,linestyle='None',c='lightgreen') 
-p6=ax2.errorbar(x+elems*3+3,data6[indexes,7],yerr=data6[indexes,13],lw=1,fmt='o',ms=2,
+p7=ax1.errorbar(x+elems*3+3,data7[indexes,6],yerr=data7[indexes,12],lw=1,fmt='o',ms=2,
                 elinewidth=1,capsize=5,linestyle='None',c='darkgreen') 
 
-p7=ax2.errorbar(x+elems*4+4,data7[indexes,7],yerr=data7[indexes,13],lw=1,fmt='o',ms=2,
+p8=ax1.errorbar(x+elems*4+4,data8[indexes,6],yerr=data8[indexes,12],lw=1,fmt='o',ms=2,
                 elinewidth=1,capsize=5,linestyle='None', c='orange') 
+
+
+
+ax2.errorbar(x,data1[indexes,7],yerr=data1[indexes,13],lw=1,fmt='o',ms=2,
+             elinewidth=1,capsize=5,linestyle='None', c='b') 
+ax2.errorbar(x,data2[indexes,7],yerr=data2[indexes,13],lw=1,fmt='o',ms=2,
+             elinewidth=1,capsize=5,linestyle='None', c='darkblue') 
+
+ax2.errorbar(x+elems*1+1,data3[indexes,7],yerr=data3[indexes,13],lw=1,fmt='o',ms=2,
+             elinewidth=1,capsize=5,linestyle='None',c='r') 
+ax2.errorbar(x+elems*1+1,data4[indexes,7],yerr=data4[indexes,13],lw=1,fmt='o',ms=2,
+             elinewidth=1,capsize=5,linestyle='None', c='darkred') 
+
+ax2.errorbar(x+elems*2+2,data5[indexes,7],yerr=data5[indexes,13],lw=1,fmt='o',ms=2,
+             elinewidth=1,capsize=5,linestyle='None', c='purple')
+ 
+ax2.errorbar(x+elems*3+3,data6[indexes,7],yerr=data6[indexes,13],lw=1,fmt='o',ms=2,
+             elinewidth=1,capsize=5,linestyle='None',c='lightgreen') 
+ax2.errorbar(x+elems*3+3,data7[indexes,7],yerr=data7[indexes,13],lw=1,fmt='o',ms=2,
+             elinewidth=1,capsize=5,linestyle='None',c='darkgreen') 
+
+ax2.errorbar(x+elems*4+4,data8[indexes,7],yerr=data8[indexes,13],lw=1,fmt='o',ms=2,
+             elinewidth=1,capsize=5,linestyle='None', c='orange') 
 
 
 #place a label in the plot
 #ax1.text(0.2,0.1, r"$z=4.0$", fontsize=22, color='k',transform=ax1.transAxes)
 
 #legend
-ax1.legend([p1,p2,p3,p4,p5,p6,p7],
+ax1.legend([p1,p2,p3,p4,p5,p8,p6,p7],
            [r"${\rm Gadget}$",
+            r"${\rm Gadget\,\,HR}$",
             r"${\rm PKDGrav}$",
             r"${\rm PKDGrav\,\,HR}$",
             r"${\rm Abacus}$",
+            r"${\rm CUBEP3M}$",
             r"${\rm Ramses}$",
-            r"${\rm Ramses\,\,HR}$",
-            r"${\rm CUBEP3M}$"],
+            r"${\rm Ramses\,\,HR}$"],
            loc=0,prop={'size':14},ncol=4,frameon=True, bbox_to_anchor=(0.1, 0.97))
             
             #columnspacing=2,labelspacing=2)
