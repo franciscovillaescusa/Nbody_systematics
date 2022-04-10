610a9bf269e716496463ab8b414b3efe92d66a77
franciscovillaescusa <villaescusa.francisco@gmail.com>
2022-04-10 09:31:29 -0400

    update

--- plots/inference_same_seed/plot.py
+++ plots/inference_same_seed/plot.py
@@ -117,7 +117,7 @@ rcParams["mathtext.fontset"]='cm'
 #ax1.add_artist(polygon)
 ####################################################################
 
-x_min, x_max = -2, 80
+x_min, x_max = -2, 96
 y_min, y_max = 0.05, 0.45
 
 
@@ -159,6 +159,7 @@ f5 = '%s/Trained_Gadget_tested_Abacus_z=0.00.txt'%root
 f6 = '%s/Trained_Gadget_tested_Ramses_z=0.00.txt'%root
 f7 = '%s/Trained_Gadget_tested_Ramses_HR_z=0.00.txt'%root
 f8 = '%s/Trained_Gadget_tested_CUBEP3M_nc1024_pp2_z=0.00.txt'%root
+f9 = '%s/Trained_Gadget_tested_Gizmo_z=0.00.txt'%root
 
 data1 = np.loadtxt(f1) 
 data2 = np.loadtxt(f2) 
@@ -168,6 +169,7 @@ data5 = np.loadtxt(f5)
 data6 = np.loadtxt(f6) 
 data7 = np.loadtxt(f7) 
 data8 = np.loadtxt(f8) 
+data9 = np.loadtxt(f9) 
 
 elems = 15
 x = np.arange(elems)
@@ -194,6 +196,9 @@ p7=ax1.errorbar(x+elems*3+3,data7[indexes,6],yerr=data7[indexes,12],lw=1,fmt='o'
 p8=ax1.errorbar(x+elems*4+4,data8[indexes,6],yerr=data8[indexes,12],lw=1,fmt='o',ms=2,
                 elinewidth=1,capsize=5,linestyle='None', c='orange') 
 
+p9=ax1.errorbar(x+elems*5+5,data9[indexes,6],yerr=data9[indexes,12],lw=1,fmt='o',ms=2,
+                elinewidth=1,capsize=5,linestyle='None', c='brown') 
+
 
 
 ax2.errorbar(x,data1[indexes,7],yerr=data1[indexes,13],lw=1,fmt='o',ms=2,
@@ -217,21 +222,25 @@ ax2.errorbar(x+elems*3+3,data7[indexes,7],yerr=data7[indexes,13],lw=1,fmt='o',ms
 ax2.errorbar(x+elems*4+4,data8[indexes,7],yerr=data8[indexes,13],lw=1,fmt='o',ms=2,
              elinewidth=1,capsize=5,linestyle='None', c='orange') 
 
+ax2.errorbar(x+elems*5+5,data9[indexes,7],yerr=data9[indexes,13],lw=1,fmt='o',ms=2,
+             elinewidth=1,capsize=5,linestyle='None', c='brown') 
+
 
 #place a label in the plot
 #ax1.text(0.2,0.1, r"$z=4.0$", fontsize=22, color='k',transform=ax1.transAxes)
 
 #legend
-ax1.legend([p1,p2,p3,p4,p5,p8,p6,p7],
+ax1.legend([p1,p2,p3,p4,p6,p7,p5,p8,p9],
            [r"${\rm Gadget}$",
             r"${\rm Gadget\,\,HR}$",
             r"${\rm PKDGrav}$",
             r"${\rm PKDGrav\,\,HR}$",
+            r"${\rm Ramses}$",
+            r"${\rm Ramses\,\,HR}$",
             r"${\rm Abacus}$",
             r"${\rm CUBEP3M}$",
-            r"${\rm Ramses}$",
-            r"${\rm Ramses\,\,HR}$"],
-           loc=0,prop={'size':14},ncol=4,frameon=True, bbox_to_anchor=(0.1, 0.97))
+            r"${\rm Gizmo}$"],
+           loc=0,prop={'size':14},ncol=5,frameon=True, bbox_to_anchor=(0.04, 0.97))
             
             #columnspacing=2,labelspacing=2)
 
